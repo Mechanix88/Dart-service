c196c4bd610926aac011dc4838994ae492eb59a6
mehcnaix96 <Mechanix96@mail.ru>
2019-06-21 19:01:14 +0300

    Адапатив

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+var accordeon = $(".accordeon__item")
+accordeon.on('click', function(){
+    $(this).toggleClass("accordeon--active")
+})
+
+
+
+
+var avatar = $(".team__link")
+var avatar2 = $('.team__item')
+var slider = $(".slider__item")
+avatar.on('click', function(e){
+    e.preventDefault()
+    slider.hide()
+    var show = $(this).attr('href')
+    $(show).fadeIn()
+})
+avatar2.on('click' , function(){
+    $(this).addClass("team__item--active").siblings().removeClass('team__item--active')
+})
+})
+
+
+var burger = $(".burger-menu__button")
+var burgerMenu = $('.burger-menu')
+burgerMenu.on('click' , function(e){
+    e.preventDefault()
+    burger.toggleClass('burger-menu_button--active')
+})
+
+var burgerWrapper = $(".burger__wrapper")
+burgerMenu.on('click' , function(e){
+    e.preventDefault()
+    burgerWrapper.toggleClass('burger__wrapper--active')
+})
